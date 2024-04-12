054ae3971ec5573eb90fa676bd9b850e8e97c55f
Crogen0407 <mymcoding0407@gmail.com>
2024-04-12 23:33:27 +0900

    feat : Custom Editor Complete

---
+++ Assets/Editor/ResourceManagementWindowEditor/EditorExtension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class EditorExtension : MonoBehaviour
+    {
+        public static T CreateScriptableObject<T>(string path) where T : ScriptableObject
+        {
+            string uniquePath = AssetDatabase.GenerateUniqueAssetPath(path);
+        
+            T newResourceManagementWindowSaveData = ScriptableObject.CreateInstance<T>();
+            T newSO = newResourceManagementWindowSaveData;
+            AssetDatabase.CreateAsset(newResourceManagementWindowSaveData, uniquePath); 
+        
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            return newSO;
+        }
+    }
+}
