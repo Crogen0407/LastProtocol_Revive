ba21dc45f886beed22942465ef92ea90a06a2efb
dongdong5899 <dong012673@gmail.com>
2024-04-13 14:07:38 +0900

    .

--- Assets/02_Stript/KDR/Satellite.cs
+++ Assets/02_Stript/KDR/Satellite.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class Satellite : ResourceStorage
 {
+    [SerializeField]
+    private Resource makeResource;
+    [SerializeField]
+    private float makeTime = 1f;
+    private float currentMakeTime = 0f;
 
+    private bool isMakeable;
+
+    private void Update()
+    {
+        if (isMakeable)
+        {
+            if (currentMakeTime > 0)
+            {
+                currentMakeTime -= Time.deltaTime;
+            }
+            else
+            {
+                Make();
+            }
+        }
+    }
+    public void Initialize()
+    {
+        makeResource = Resource.None;
+    }
+
+    public void ChangeMakeResource(Resource resource)
+    {
+        makeResource = resource;
+        //makeTime = resource.makingTime;
+        currentMakeTime = 0;
+    }
+
+    private void Make()
+    {
+        /*if (GetResource())
+        {
+            AddResource(makeResource, 1);
+            currentMakeTime = makeTime;
+        }*/
+    }
 }
