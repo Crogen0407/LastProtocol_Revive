6d0e8aff03db17073e9afea922d62859827b272f
dongdong5899 <dong012673@gmail.com>
2024-05-14 01:49:44 +0900

    .

--- Assets/02_Stript/KDR/Selectable.cs
+++ Assets/02_Stript/KDR/Selectable.cs
@@ -18,7 +18,6 @@ public class Selectable : MonoBehaviour
     public virtual void EnterCursor()
     {
         material.SetInt(onSelectMatHash, 1);
-        Debug.Log(1);
     }
     public virtual void StayCursor()
     {
@@ -27,11 +26,10 @@ public class Selectable : MonoBehaviour
     public virtual void ExitCursor()
     {
         material.SetInt(onSelectMatHash, 0);
-        Debug.Log(0);
     }
     public virtual void MouseDown(Vector2 mousePos)
     {
-        Debug.Log(transform.gameObject.name);
+
     }
     public virtual void MouseUp(Vector2 mousePos)
     {
